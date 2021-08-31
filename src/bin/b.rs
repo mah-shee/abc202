5c4d20d48be2ac013e6a422d83eb5c816fc52d4b
mah-shee <masa@masatoshi.local>
2021-08-31 23:07:42 +0900

    Done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -9,7 +9,6 @@ fn main() {
     let ans: String = s
         .iter()
         .map(|&a| match a {
-            '0' | '1' | '8' => a,
             '6' => '9',
             '9' => '6',
             _ => a,
