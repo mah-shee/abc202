f7d1c53bd8733c9ba4c72215d6c169a8ce3e3325
mah-shee <masa@masatoshi.local>
2021-08-31 23:06:55 +0900

    Done b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -4,5 +4,17 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        mut s: Chars
     }
+    let ans: String = s
+        .iter()
+        .map(|&a| match a {
+            '0' | '1' | '8' => a,
+            '6' => '9',
+            '9' => '6',
+            _ => a,
+        })
+        .rev()
+        .collect();
+    println!("{}", ans);
 }
