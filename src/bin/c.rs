74b8b997737fabb9bae8098d7ef8023cfdb5be60
mah-shee <masa@masatoshi.local>
2021-08-31 23:20:38 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,18 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n:usize,
+        a:[usize; n],
+        b:[usize;n],
+        c:[usize;n],
     }
+    let mut count = vec![0; n];
+    for i in 0..n {
+        count[b[c[i] - 1] - 1] += 1;
+    }
+    let mut ans: u128 = 0;
+    for i in 0..n {
+        ans += count[a[i] - 1]
+    }
+    println!("{}", ans);
 }
