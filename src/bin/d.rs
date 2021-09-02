172b321ac547f3837c0711c3a81107769ad28def
mah-shee <masa@masatoshi.local>
2021-09-02 23:48:03 +0900

    Done d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -4,5 +4,37 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        mut a: usize,
+        mut b: usize,
+        mut k: u128
+    }
+    // a = 2, b = 2, k = 4, 0011, 0101
+    let mut dp = vec![vec![0; b + 1]; a + 1];
+    dp[0][0] = 1;
+    for i in 0..=a {
+        for j in 0..=b {
+            if i > 0 {
+                dp[i][j] += dp[i - 1][j];
+            }
+            if j > 0 {
+                dp[i][j] += dp[i][j - 1];
+            }
+        }
+    }
+    while a > 0 && b > 0 {
+        if k <= dp[a - 1][b] {
+            print!("a");
+            a -= 1;
+        } else {
+            k -= dp[a - 1][b];
+            print!("b");
+            b -= 1;
+        }
+    }
+    for _ in 0..a {
+        print!("a");
+    }
+    for _ in 0..b {
+        print!("b");
     }
 }
